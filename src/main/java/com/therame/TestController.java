b61aaff0977d0ca6301c37e1f770b9b509ba129f
Jay Ricco <riccoj@wit.edu>
2018-01-17 10:56:42 -0500

    Added basic spring boot application with a few of the tests from:
    https://spring.io/guides/gs/spring-boot/

---
+++ src/main/java/com/therame/TestController.java
@@ -0,0 +1,13 @@
+package com.therame;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spongbeb.";
+    }
+}
