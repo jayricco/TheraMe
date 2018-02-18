7a0c120155a944ee9a702e2dfdb889c280be3656
Joe12o <joe.298@live.com>
2018-02-18 17:43:25 -0500

    Add a progress indicator for uploads. Also some minor button styling changes.

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -3,11 +3,25 @@ $(document).ready(function () {
     $('form').submit(function(event) {
         event.preventDefault();
 
+        var submitButton = $('#upload-submit');
+
+        // Make sure the user doesn't submit another while already uploading
+        if (submitButton.hasClass('disabled')) {
+            return;
+        }
+
         var successMessage = $('#success-message');
         successMessage.html('').hide();
 
         var errorMessage = $('#error-message');
         errorMessage.html('').hide();
+
+        var progressBar = $('#upload-progress');
+        progressBar.removeClass('hidden');
+
+        var progressIndicator = progressBar.find('.progress-bar');
+
+        submitButton.addClass('disabled');
 
         var formData = new FormData($('form')[0]);
 
@@ -18,22 +32,37 @@ $(document).ready(function () {
             processData: false,
             contentType: false,
             cache: false,
+            xhr: function() {
+                var xhr = $.ajaxSettings.xhr();
+                xhr.upload.addEventListener('progress', function(event) {
+                    if (event.lengthComputable) {
+                        var percentComplete = (event.loaded / event.total) * 100;
+                        progressIndicator.css({ width: percentComplete + '%' });
+                        progressIndicator.attr('aria-valuenow', percentComplete);
+                    }
+                });
+                return xhr;
+            },
             success: function() {
                 successMessage.show().html('Video uploaded successfully!');
+                submitButton.removeClass('disabled');
             },
             error: function(response) {
                 var errorString = '';
 
-                if (response.responseJSON.errors) {
-                    response.responseJSON.errors.forEach(function (error) {
-                        $('#' + error.field).addClass('is-invalid');
-                        errorString += '- ' + error.defaultMessage + '\n';
-                    });
-                } else if (response.responseJSON.message) {
-                    errorString = '- ' + response.responseJSON.message;
+                if (response.responseJSON) {
+                    if (response.responseJSON.errors) {
+                        response.responseJSON.errors.forEach(function (error) {
+                            $('#' + error.field).addClass('is-invalid');
+                            errorString += '- ' + error.defaultMessage + '\n';
+                        });
+                    } else if (response.responseJSON.message) {
+                        errorString = '- ' + response.responseJSON.message;
+                    }
                 }
 
-                errorMessage.show().html(errorString)
+                errorMessage.show().html(errorString);
+                submitButton.removeClass('disabled');
             }
         });
     });
